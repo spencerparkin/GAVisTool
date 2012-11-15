dc5f85dbc90f2292509415b5187f27e9a2c7b8d5
Spencer <Spencer.Parkin@disney.com>
2012-11-15 16:58:50 -0700

    header file

---
+++ Code/WinApp/VectorMath/GiftWrapper.h
@@ -0,0 +1,28 @@
+// GiftWrapper.h
+
+/*
+ * Copyright (C) 2012 Spencer T. Parkin
+ *
+ * This software has been released under the MIT License.
+ * See the "License.txt" file in the project root directory
+ * for more information about this license.
+ *
+ */
+
+#pragma once
+
+#include "Vector.h"
+
+namespace VectorMath
+{
+	// This class implements a simple variation of the gift wrapping algorithm.
+	// Without a given neighborhood, it is the gift wrapping algorithm, and can
+	// find convex hulls of point clouds.  With a given neighborhood, it can find
+	// what one might consider the concave hull of a given point cloud.
+	class GiftWrapper
+	{
+	public:
+	};
+}
+
+// GiftWrapper.h
